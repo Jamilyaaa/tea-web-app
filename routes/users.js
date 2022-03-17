2b15fa59786b847b58c6278eb2d0dfc0794dc7be
dzhamilia <orujeva_jamilya@mail.ru>
2022-03-17 16:31:12 +0300

    routes/applicaion/fetch

--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,70 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const sha256 = require('sha256');
+const sha256 = require("sha256");
 
-const { User } = require('../db/models');
-const { userChecker, deepCheckUser, showBody } = require('./middleWare/middlewares');
+const { User } = require("../db/models");
+const {
+  userChecker,
+  deepCheckUser,
+  showBody,
+} = require("./middleWare/middlewares");
 
 /* GET users listing. */
-router.get('/registration', (req, res) => {
-  res.render('registration');
+router.get("/registration", (req, res) => {
+  res.render("registration");
 });
 
-router.post('/registration', async (req, res) => {
+router.post("/registration", async (req, res) => {
   const { login, email } = req.body;
   const password = sha256(req.body.password); // шифруем пароль
-  const user = await User.create({ role: false, login, email, password, isBanned:false }); // создаем нового юзера
+  const user = await User.create({
+    role: false,
+    login,
+    email,
+    password,
+    isBanned: false,
+  }); // создаем нового юзера
   req.session.userLogin = user.login; // записываем в сессию имя юзера, чтобы потмо его проверять (см в middlewares.js)
   req.session.userEmail = user.email;
   req.session.userId = user.id;
-  res.render('ok')
-  //res.redirect(`users/lk/${user.id}`); // редирект на профиль нового юзера
+  res.json({ id: user.id });
+  // res.redirect(`/users/lk/${user.id}`); // редирект на профиль нового юзера
 });
 
-router.get('/login', (req, res) => {
-  res.render('loggin');
+router.get("/login", (req, res) => {
+  res.render("loggin");
 });
 
-router.post('/login', showBody, async (req, res) => {
+router.post("/login", showBody, async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ where: { email } }); // ищем в бд юзера по почте
   if (!user) {
-    return res.redirect('/users/registration');
+    return res.redirect("/users/registration");
   }
-  if (user.password === sha256(req.body.password)) { // если шифрованный пароль из бд совпадает с зашифрованным тем что ввел юзер
+  if (user.password === sha256(req.body.password)) {
+    // если шифрованный пароль из бд совпадает с зашифрованным тем что ввел юзер
     req.session.userLogin = user.login; // записываем в сессию имя юзера, чтобы потмо его проверять (см middlewares.js)
     req.session.userEmail = user.email;
     req.session.userId = user.id;
     res.redirect(`/users/lk/${user.id}`);
   } else {
-    res.send('wrong password');
+    res.send("wrong password");
   }
 });
 
-router.get('/lk/:id', userChecker, deepCheckUser, async (req, res) => { // проходим мидлверы и попадаем в профиль
+router.get("/lk/:id", userChecker, deepCheckUser, async (req, res) => {
+  // проходим мидлверы и попадаем в профиль
   const user = await User.findByPk(req.params.id);
-  res.render('lk', { user });
+  res.render("lk", { user });
 });
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   // при logout сессия удаляется из папки sessions
   req.session.destroy();
-  res.clearCookie('userCookie');
-  res.redirect('/');
+  res.clearCookie("userCookie");
+  res.redirect("/");
 });
 
 module.exports = router;
