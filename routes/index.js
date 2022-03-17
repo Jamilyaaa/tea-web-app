0c511d7d85986e9e62f16f041b96081cc33c8f2d
Artem Kashuta <kashuta@gmail.com>
2022-03-17 13:42:45 +0300

    Middleware for auth added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
-
-const { userChecker } = require('../');
+const { userChecker } = require('./middleWare/middlewares');
 
 
 router.get('/', (req, res, next) => {
