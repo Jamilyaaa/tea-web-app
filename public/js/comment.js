6cf704514a923bd1de99ff7549f96c6a0b8e9261
dzhamilia <orujeva_jamilya@mail.ru>
2022-03-18 16:09:36 +0300

    add comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,8 +2,10 @@ const addComment = document.querySelector("#addComment");
 
 addComment.addEventListener("submit", async (event) => {
   event.preventDefault();
-  const { commnet } = event.target;
-  const response = await fetch("/tea/:id", {
+  const body_text = event.target.commnet;
+  const cactus = window.location.href.split("/");
+  const id = cactus[cactus.length - 1];
+  const response = await fetch(`/users/tea/${id}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,6 +16,6 @@ addComment.addEventListener("submit", async (event) => {
   });
   const data = await response.json();
   if (response.ok) {
-
-  }
+    document.getElementById("textComment").innerText = data.body_text;
+  } else alert("oops");
 });
--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,28 @@
-const express = require("express");
-const { user } = require("pg/lib/defaults");
+const express = require('express');
 
 const router = express.Router();
 
-const sha256 = require("sha256");
+const sha256 = require('sha256');
 
-const { User, Comment } = require("../db/models");
+const { User, Comment } = require('../db/models');
 
 const {
   userChecker,
   deepCheckUser,
-} = require("./middleWare/middlewares");
+} = require('./middleWare/middlewares');
 
-router.get("/registration", (req, res) => {
-  res.render("registration");
+router.get('/registration', (req, res) => {
+  res.render('registration');
 });
 
-router.post("/registration", async (req, res) => {
+router.post('/registration', async (req, res) => {
   const { login, email } = req.body;
   const password = sha256(req.body.password); // шифруем пароль
   const existUser = await User.findOne({ where: { email }, raw: true });
   if (existUser) {
     res.status(500).json({
       message:
-        "a user with such an email is already registered in the database",
+        'a user with such an email is already registered in the database',
     });
   } else {
     try {
@@ -40,22 +39,22 @@ router.post("/registration", async (req, res) => {
       res.json({ id: user.id });
       // res.redirect(`/users/lk/${user.id}`); // редирект на профиль нового юзера
     } catch (error) {
-      res.status(400).json({ message: "ooops" });
+      res.status(400).json({ message: 'ooops' });
     }
   } // создаем нового юзера
 });
 
-router.get("/login", (req, res) => {
-  res.render("loggin");
+router.get('/login', (req, res) => {
+  res.render('loggin');
 });
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
     const { email } = req.body;
     const user = await User.findOne({ where: { email } }); // ищем в бд юзера по почте
     if (!user) {
-      return res.status(504).json({message: `there is no such user`});
-    } 
+      return res.status(504).json({ message: 'there is no such user' });
+    }
     if (user.password === sha256(req.body.password)) {
       // если шифрованный пароль из бд совпадает с зашифрованным тем что ввел юзер
       req.session.userLogin = user.login; // записываем в сессию имя юзера, чтобы потмо его проверять (см middlewares.js)
@@ -64,40 +63,41 @@ router.post("/login", async (req, res) => {
       res.json({ id: user.id });
       // res.redirect(`/users/lk/${user.id}`);
     } else {
-      res.send("wrong password");
+      res.send('wrong password');
     }
   } catch (error) {
-    res.status(400).json({ message: "ooopsey" });
+    res.status(400).json({ message: 'ooopsey' });
   }
 });
 
-router.get("/lk/:id", userChecker, deepCheckUser, async (req, res) => {
+router.get('/lk/:id', userChecker, deepCheckUser, async (req, res) => {
   // проходим мидлверы и попадаем в профиль
   const user = await User.findByPk(req.params.id);
   if (!user.role) {
-  res.render("lk", { user: user.login });
-  } else res.render("adminLk", { user: user.login });
+    res.render('lk', { user: user.login });
+  } else res.render('adminLk', { user: user.login });
 });
 
-router.get("/logout", (req, res) => {
+router.get('/logout', (req, res) => {
   // при logout сессия удаляется из папки sessions
   req.session.destroy();
-  res.clearCookie("tea_auth");
-  res.redirect("/");
+  res.clearCookie('tea_auth');
+  res.redirect('/');
 });
 
-//add comments
-router.post("/tea/:id", async (req,res)=>{
-  const { body_text} = req.body;
-  try { 
+// add comments
+router.post('/tea/:id', async (req, res) => {
+  const { body_text } = req.body;
+  console.log(body_text);
+  try {
     const comm = await Comment.create({
-    user_id: req.session.userLogin,
-    body_text,
-    tea_id:
-  });
-  res.send({body_text})
+      user_id:req.session.userId,
+      body_text,
+      tea_id: req.params.id,
+    });
+    res.send({ body_text });
   } catch (error) {
-    res.status(400).json({ message: "ooops" });
+    res.status(400).json({ message: 'ooops' });
   }
 });
 
