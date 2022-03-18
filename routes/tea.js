406a16b29826d39ff8a537069f156d08f8401922
dzhamilia <orujeva_jamilya@mail.ru>
2022-03-18 16:37:49 +0300

    last commits

--- routes/tea.js
+++ routes/tea.js
@@ -6,7 +6,8 @@ const { Tea } = require("../db/models");
 
 router.get("/:id", async (req, res) => {
   const tea=await Tea.findByPk(req.params.id);
-  res.render("tea", {tea: tea.tea_name});
+  // const value = await Tea.findAll({ raw: true });
+  res.render("tea", {tea: tea.tea_name,value:tea});
 });
 
 
